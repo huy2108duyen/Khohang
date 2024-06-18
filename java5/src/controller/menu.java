1f495203879936b9c326d1414702d0a3bfa63eb9
Huy Nguyen <118646346+huy2108duyen@users.noreply.github.com>
2024-06-18 13:08:05 +0700

    Delete java5 directory

--- java5/src/controller/menu.java
+++
@@ -1,7 +0,0 @@
-package controller;
-
-import java.awt.Component;
-
-public interface menu {
-    public boolean menuPress(Component com,boolean open);
-}
--- java5/src/controller/menuselected.java
+++
@@ -1,6 +0,0 @@
-package controller;
-
-
-public interface menuselected {
-    public void menuSelected(int menuIndex,int subMenuIndex);
-}
